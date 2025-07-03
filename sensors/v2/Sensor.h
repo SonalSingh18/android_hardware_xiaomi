yaap/hardware_xiaomi

sensors: Allow Overriding UDFPS interface
- Path are overridden like this:

SOONG_CONFIG_NAMESPACES += XIAOMI_SENSORS

SOONG_CONFIG_XIAOMI_SENSORS += UDFPS_PATH
SOONG_CONFIG_XIAOMI_SENSORS_UDFPS_PATH := /sys/class/touch/touch_dev/fod_press_status

Signed-off-by: GuidixX <guidixm@gmail.com>
Co-Authored-By: cyberknight777 <cyberknight755@gmail.com>
Co-Authored-By: John Galt <RealJohnGalt@users.noreply.github.com>

sensors/v2/Sensor.h
@@ -139,13 +139,15 @@ class SingleTapSensor : public SysfsPollingOneShotSensor {
               static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 2)) {}
 };
 
+const std::string kTsUdfpsPath = UDFPS_PATH;
+
 class UdfpsSensor : public SysfsPollingOneShotSensor {
   public:
     UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
         : SysfsPollingOneShotSensor(
-                sensorHandle, callback, "/sys/class/touch/touch_dev/fod_press_status",
-                "UDFPS Sensor", "org.yaap.sensor.udfps",
-                  static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +3)) {}
+              sensorHandle, callback, kTsUdfpsPath,
+              "UDFPS Sensor", "org.yaap.sensor.udfps",
+              static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 3)) {}
     virtual void fillEventData(Event& event);
     virtual bool readFd(const int fd);
 

SonalSingh18/android_hardware_xiaomi

sensors/v2/Sensor.h
@@ -145,13 +145,14 @@ class SingleTapSensor : public SysfsPollingOneShotSensor {
                                           2)) {}
 };
 
+const std::string kTsUdfpsPath = UDFPS_PATH;
+
 class UdfpsSensor : public SysfsPollingOneShotSensor {
   public:
     UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
         : SysfsPollingOneShotSensor(
-                  sensorHandle, callback, "/sys/class/touch/touch_dev/fod_press_status",
-                  "/sys/class/touch/touch_dev/fod_longpress_gesture_enabled", "UDFPS Sensor",
-                  "org.lineageos.sensor.udfps",
+                  sensorHandle, callback, kTsUdfpsPath,
+                  "UDFPS Sensor", "org.lineageos.sensor.udfps",
                   static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +
                                           3)) {}
     virtual void fillEventData(Event& event);